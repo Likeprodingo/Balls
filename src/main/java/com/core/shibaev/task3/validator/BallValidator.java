b43c1ef321b6544a0f861986e8fc9d6dc970e784
Likeprodingo <42166797+Likeprodingo@users.noreply.github.com>
2020-06-15 17:52:10 +0300

    Task3

---
+++ src/main/java/com/core/shibaev/task3/validator/BallValidator.java
@@ -0,0 +1,14 @@
+package com.core.shibaev.task3.validator;
+
+public class BallValidator {
+    public boolean addBallValidation(double radius, double weight)
+    {
+        boolean result = true;
+        if(radius<0 || weight < 0 || radius > 4 || weight > 30)
+        {
+            result = false;
+        }
+        return result;
+    }
+
+}
